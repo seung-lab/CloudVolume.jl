3e6f7188600d00440d45c4548d7339a6741eee80
Thomas Macrina <tmacrina@princeton.edu>
2017-08-31 16:43:01 -0400

    Include initial CloudVolumeWrapper
    * Add CloudVolumeWrapper from cloud-volume's julia-wrapper branch
    * Include export and extend init for all args
    * Setup README.md

--- README.md
+++ README.md
@@ -5,3 +5,30 @@
 [![Coverage Status](https://coveralls.io/repos/macrintr/CloudVolume.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/macrintr/CloudVolume.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/macrintr/CloudVolume.jl/coverage.svg?branch=master)](http://codecov.io/github/macrintr/CloudVolume.jl?branch=master)
+
+Julia wrapper for cloud-volume from Python.
+
+## Installation  
+Install cloud-volume, e.g.
+```
+pip install cloud-volume  
+```
+
+Clone this package,
+```
+Pkg.clone("https://github.com/seung-lab/CloudVolume.jl")
+```
+
+## Quickstart
+Note that indexing is inclusive, like Julia.
+```
+using CloudVolume
+vol = CloudVolumeWrapper("path location to precomputed files")
+img = vol[1000:1100, 2000:2100, 100:200]  # download images
+vol[1000:1100, 2000:2100, 100:200] = img  # upload images
+```
+
+Note that uploaded data must be chunk-aligned. For more details, see the cloud-volume documentation](https://github.com/seung-lab/cloud-volume#cloud-volume).
+
+## Credits
+Thanks to @jonathanzung for an earlier version of this wrapper.
--- src/CloudVolume.jl
+++ src/CloudVolume.jl
@@ -1,5 +1,57 @@
+"""
+Credit to @jonathanzung for an earlier Precomputed version
+"""
+
 module CloudVolume
 
-# package code goes here
+export CloudVolumeWrapper
 
-end # module
+using PyCall
+@pyimport cloudvolume as cv
+const pyslice=pybuiltin(:slice)
+
+function cached(f)
+	cache=Dict()
+	function my_f(args...)
+		if !haskey(cache, args)
+			cache[args] = f(args...)
+		else
+			println("restoring from cache")
+		end
+		return cache[args]
+	end
+end
+
+CachedVolume = cached(cv.CloudVolume)
+
+immutable CloudVolumeWrapper
+	val
+	function CloudVolumeWrapper(storage_string, scale_idx=0,
+						bounded=true,
+						fill_missing=false,
+						info=nothing)
+		return new(CachedVolume(storage_string, scale_idx, 
+						bounded, 
+						fill_missing, 
+						info))
+	end
+end
+
+function Base.getindex(x::CloudVolumeWrapper, slicex::UnitRange, 
+                                        slicey::UnitRange, slicez::UnitRange)
+    return squeeze(get(x.val, 
+            (pyslice(slicex.start,slicex.stop+1),
+            pyslice(slicey.start,slicey.stop+1),
+            pyslice(slicez.start,slicez.stop+1))),4)
+end
+
+function Base.setindex!(x::CloudVolumeWrapper, img::Array, slicex::UnitRange, 
+                                        slicey::UnitRange, slicez::UnitRange)
+    x.val[:__setitem__]((pyslice(slicex.start,slicex.stop+1),
+                            pyslice(slicey.start,slicey.stop+1),
+                            pyslice(slicez.start,slicez.stop+1)), 
+                            img)
+end
+
+end # module CloudVolume
+
