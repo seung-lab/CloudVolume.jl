69078f91bfe1608eb48b013a9e8bd6c33824dc36
jingpengwu <jingpeng.wu@gmail.com>
2017-11-09 16:20:06 -0500

    add option for 1-based indexing; update README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # CloudVolume
 
-[![Build Status](https://travis-ci.org/macrintr/CloudVolume.jl.svg?branch=master)](https://travis-ci.org/macrintr/CloudVolume.jl)
+[![Build Status](https://travis-ci.org/seung-lab/CloudVolume.jl.svg?branch=master)](https://travis-ci.org/seung-lab/CloudVolume.jl)
 
-[![Coverage Status](https://coveralls.io/repos/macrintr/CloudVolume.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/macrintr/CloudVolume.jl?branch=master)
+[![Coverage Status](https://coveralls.io/repos/seung-lab/CloudVolume.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/seung-lab/CloudVolume.jl?branch=master)
 
-[![codecov.io](http://codecov.io/github/macrintr/CloudVolume.jl/coverage.svg?branch=master)](http://codecov.io/github/macrintr/CloudVolume.jl?branch=master)
+[![codecov.io](http://codecov.io/github/seung-lab/CloudVolume.jl/coverage.svg?branch=master)](http://codecov.io/github/seung-lab/CloudVolume.jl?branch=master)
 
 Julia wrapper for cloud-volume from Python.
 
@@ -24,14 +24,29 @@ using CloudVolume
 vol = CloudVolumeWrapper("<path to precomputed file directory>")
 img = vol[1000:1100, 2000:2100, 100:200]  # download images
 vol[1000:1100, 2000:2100, 100:200] = img  # upload images
+```
+
+There is also an option for 1-based indexing. This is more like Julia array, in which the first element index is 1. And also, this is inclusive.
+```
+using CloudVolume
+# CloudVolume object
+vol = CloudVolumeWrapper("<path to precomputed file directory>")
+img = vol[1:256, 1:256, 1:16]  # download images
+vol[1:256, 1:256, 1:16] = img  # upload images
+```
+
+Note that uploaded CloudVolume data must be chunk-aligned. For more details, see the [cloud-volume documentation](https://github.com/seung-lab/cloud-volume#cloud-volume).
+
+
+### Strorage Interface
+there is also a stroage interface to store any objects, including text files.
+```
 # Storage object
 s = StorageWrapper("<path to storage directory>")
 s["filename"] = "content"
 s["filename"] == "content" # returns true
 delete!(s, "filename")
 ```
-
-Note that uploaded CloudVolume data must be chunk-aligned. For more details, see the [cloud-volume documentation](https://github.com/seung-lab/cloud-volume#cloud-volume).
 
 ## Troubleshotting  
 If you installed the CloudVolume Python package inside a virtualenv, you many need to rebuild `PyCall` to use that virtualenv.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,52 @@
 using CloudVolume
 using Base.Test
 
-path = "gs://seunglab/jpwu/test/image/"
-ba = CloudVolumeWrapper(path)
-
-a = rand(UInt8, 256,256,16)
-
 @testset "test 3D image reading and saving" begin  
-    # ba = BigArray(d, UInt8, (128,128,8))
-    @time ba[257:512, 257:512, 17:32] = a
-    # BigArrays.mysetindex!(ba, a, (201:400, 201:400, 161:116))
-    @time b = ba[257:512, 257:512, 17:32]
+    path = "gs://seunglab/jpwu/test/image/"
+    vol = CloudVolumeWrapper(path)
+    a = rand(UInt8, 256,256,16)
+    @time vol[256:511, 256:511, 16:31] = a
+    @time b = vol[256:511, 256:511, 16:31]
     @test all(a.==b)
 end
 
-@testset "test single voxel indexing" begin 
-    x = a[1,1,1]
-    y = ba[257,257,17]
-    @test x==y
-end 
+@testset "test 3D image IO with 1-based indexing" begin  
+    path = "gs://seunglab/jpwu/test/image/"
+    vol = CloudVolumeWrapper(path; is1based=true)
+    a = rand(UInt8, 256,256,16)
+    @time vol[257:512, 257:512, 17:32] = a
+    @time b = vol[257:512, 257:512, 17:32]
+    @test all(a.==b)
+end
 
 @testset "test segmentation" begin 
     path = "gs://seunglab/jpwu/test/segmentation/"
-    # ba = BigArray( d, configDict )
-    ba = CloudVolumeWrapper(path)
+    # vol = BigArray( d, configDict )
+    vol = CloudVolumeWrapper(path; is1based=true)
     a = rand(UInt32, 256,256,16)
-    @time ba[257:512, 257:512, 17:32] = a
-    @time b = ba[257:512, 257:512, 17:32]
+    @time vol[257:512, 257:512, 17:32] = a
+    @time b = vol[257:512, 257:512, 17:32]
     @test all(a.==b)
 end 
 
 
 @testset "test segmenation with uint64" begin 
     path = "gs://seunglab/jpwu/test/segmentation-uint64/"
-    ba = CloudVolumeWrapper(path)
+    vol = CloudVolumeWrapper(path; is1based=true)
     a = rand(UInt64, 256,256,16)
-    @time ba[257:512, 257:512, 17:32] = a
-    @time b = ba[257:512, 257:512, 17:32]
+    @time vol[257:512, 257:512, 17:32] = a
+    @time b = vol[257:512, 257:512, 17:32]
     @test all(a.==b)
 end 
 
 
 @testset "test affinity map" begin 
     path = "gs://seunglab/jpwu/test/affinitymap/"
-    ba = CloudVolumeWrapper(path)
+    vol = CloudVolumeWrapper(path; is1based=true )
 
     a = rand(Float32, 256,256,16,3)
-    @time ba[257:512, 257:512, 17:32, 1:3] = a
-    @time b = ba[257:512, 257:512, 17:32, 1:3]
+    @time vol[257:512, 257:512, 17:32] = a
+    @time b = vol[257:512, 257:512, 17:32]
 
     @show size(a)
     @show size(b)
@@ -57,10 +56,10 @@ end
 @testset "test semantic map" begin 
     path = "gs://seunglab/jpwu/test/semanticmap/" 
     a = rand(Float32, 256,256,16,4)
-    ba = CloudVolumeWrapper(path)
+    vol = CloudVolumeWrapper(path; is1based=true)
 
-    @time ba[257:512, 257:512, 17:32, 1:4] = a
-    @time b = ba[257:512, 257:512, 17:32, 1:4]
+    @time vol[257:512, 257:512, 17:32] = a
+    @time b = vol[257:512, 257:512, 17:32]
 
     @show size(a)
     @show size(b)
